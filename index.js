c41e9fb1a2a03855bc29dbc81bb2a683dadd9aba
RajGM <rajgmsocial19@gmail.com>
2020-10-07 16:20:14 +0530

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 //var database = require('./database');
 var mongo = require('mongodb');
+const mongoose = require("mongoose");
 
 // Define our application
 const app = express();
@@ -12,38 +13,26 @@ app.set('port', process.env.PORT || 3000);
 var route = require('./router');
 app.use('/', route);
 
-
-
+/*
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://RajGM:Gaurav@123todatabase@cluster0.mwzax.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true ,  useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
+  console.log(err);
   client.close();
 });
-
-
-
-// Connect to the database, then execute the "callback" function (in this case an anonymous function which starts listening for requests).
-// Start listening for requests on the port defined earlier
-
-/*
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    var myobj = { name: "Company Inc", address: "Highway 37" };
-    dbo.collection("customers").insertOne(myobj, function(err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-      db.close();
-    });
-  });
 */
 
+const configFile = require('./myUrl.js');
+const db = configFile.mongoURL+configFile.userName+":"+configFile.password+configFile.restUrl;
+
+
+mongoose
+  .connect(db)
+  .then(()=>console.log("Connected Successfully"))
+  .catch(err=>console.log(err));
 
     app.listen(app.get('port'), function(){
         console.log("Express server listening on port " + app.get('port'));
---
+++ myUrl.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mongoURL: "mongodb+srv://",
+    userName:"testUser",
+    password:"hackOntheWayCapstone",
+    restUrl:"@cluster0.mwzax.mongodb.net/<dbname>?retryWrites=true&w=majority"
+  };
+  
+
+  //format
+  //restUrl:"mongodb+srv://RajGM:Gaurav@123todatabase@cluster0.mwzax.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    
