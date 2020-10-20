370768cbddf7872d354d344240f02242964964ee
RajGM <rajgmsocial19@gmail.com>
2020-10-21 00:47:01 +0530

    login send and recieve 400 and 404 error

--- public/javaScript/loginClient.js
+++ public/javaScript/loginClient.js
@@ -4,4 +4,21 @@ var LoginButton = document.getElementById("LoginButton");
 LoginButton.onclick = function () {
     console.log(userName.value);
     console.log(Password.value);
+
+    var objSent = {
+        userName:"",
+        Password:""
+    }
+
+    objSent.userName = userName;
+    objSent.Password = Password;
+
+    var jsonFormat = JSON.parse("userName="+userName+"&"+"Password="+Password);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/login/", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("userName="+userName+"&"+"Password="+Password);    
+    //xhttp.send(objSent);
+
 }
--- public/javaScript/registerClient.js
+++ public/javaScript/registerClient.js
@@ -1,8 +1,8 @@
 var userName = document.getElementById("userName");
 var Password = document.getElementById("Password");
 var CPassword = document.getElementById("cPassword");
-var LoginButton = document.getElementById("RegisterButton");
-LoginButton.onclick = function () {
+var RegisterButton = document.getElementById("RegisterButton");
+RegisterButton.onclick = function () {
     console.log(userName.value);
     console.log(Password.value);
     console.log(CPassword.value);
